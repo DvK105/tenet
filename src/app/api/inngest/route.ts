4312c97a35457e4fcb767eac21aabff12ef0a0a7
DvK <dhstudiosanimation@gmail.com>
2025-11-27 21:47:20 +0530

    12: render system

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { helloOnUpload } from "@/inngest/functions/hello";
+import { renderJob } from "@/inngest/functions/render-job";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloOnUpload,
+    renderJob,
   ],
 });
